2e9e9d456b8c35660e8897b9ed718bfc544c1383
Zaina Nizar <63456398+zainanizar@users.noreply.github.com>
2021-08-26 15:58:15 +0530

    Add files via upload

--- src/Page/Page.jsx
+++ src/Page/Page.jsx
@@ -1,4 +1,5 @@
 import "./Page.css"
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 export default function Page() {
     return (
@@ -14,10 +15,114 @@ export default function Page() {
            <div className="Page2">Journey</div>
         </div>
         <div className="PageLine2" />
-        <div className="Nutrition">
-            <div className="Package">Package</div>
-            <div className="Program">Program</div>
+        <div class="row">
+        <div class="column"></div>
+        <div class="column">Package</div>
+        <div class="column">Program</div>
         </div>
+        <br></br>
+        <br></br>
+        <table>
+            <tr >
+                <th scope="col">Type</th>
+                <th scope="col">Client</th>
+                <th scope="col">Name</th>
+                <th scope="col">Status</th>
+                <th scope="col">Start Date</th>
+                <th scope="col">Renewal</th>
+                <th scope="col">Status</th>
+                <th scope="col">Renewal</th>
+                <th scope="col">Actions</th>
+
+            </tr>
+            <tr >
+                <th scope="row">Custom Nutition</th>
+                <th scope="row">Walter White</th>
+                <th scope="row">Package Name</th>
+                <th scope="row">
+                <button class="btn success">Active</button>
+                </th>
+                <th scope="row">25/06/21</th>
+                <th scope="row">25/06/21</th>
+                <th scope="row">
+                <button class="inactive">Not Available</button>
+
+                </th>
+                <th scope="row">25/06/21</th>
+                <th scope="row">
+                    <MoreVertIcon/>
+                </th>
+
+            </tr>
+            <tr>
+                <th scope="row">Custom Nutition</th>
+                <th scope="row">Walter White</th>
+                <th scope="row">Package Name</th>
+                <th scope="row">
+                <button class="btn success">Active</button>
+                </th>
+                <th scope="row">25/06/21</th>
+                <th scope="row">25/06/21</th>
+                <th scope="row">
+                <button class="btn success">Assigned</button>
+
+                </th>
+                <th scope="row">25/06/21</th>
+                <th scope="row"><MoreVertIcon/></th>
+
+            </tr>
+            <tr>
+                <th scope="row">Custom Nutition</th>
+                <th scope="row">Walter White</th>
+                <th scope="row">Package Name</th>
+                <th scope="row">
+                <button class="btn success">Active</button>
+                </th>
+                <th scope="row">25/06/21</th>
+                <th scope="row">25/06/21</th>
+                <th scope="row">
+                <button class="btn success">Assigned</button>
+                </th>
+                <th scope="row">25/06/21</th>
+                <th scope="row"><MoreVertIcon/></th>
+
+            </tr>
+            <tr>
+                <th scope="row">Custom Nutition</th>
+                <th scope="row">Walter White</th>
+                <th scope="row">Package Name</th>
+                <th scope="row">
+                <button class="inactive">Inactive</button>
+                </th>
+                <th scope="row">25/06/21</th>
+                <th scope="row">25/06/21</th>
+                <th scope="row">
+                <button class="btn success">Assigned</button>
+
+                </th>
+                <th scope="row">25/06/21</th>
+                <th scope="row"><MoreVertIcon/></th>
+
+            </tr>
+            <tr>
+                <th scope="row">Custom Nutition</th>
+                <th scope="row">Walter White</th>
+                <th scope="row">Package Name</th>
+                <th scope="row">
+                <button class="btn success">Active</button>
+                </th>
+                <th scope="row">25/06/21</th>
+                <th scope="row">25/06/21</th>
+                <th scope="row">
+                <button class="btn success">Assigned</button>
+
+                </th>
+                <th scope="row">25/06/21</th>
+                <th scope="row"><MoreVertIcon/></th>
+
+            </tr>
+            
+        </table>
         </div>
         </div>
 
